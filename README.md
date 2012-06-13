495f767d35fa81ecc5af41c1b28def029283f8bb
Graham Higgins <gjh-github@bel-epa.com>
2012-06-13 02:45:50 +0100

    Disable Python 3, badly broken.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 MySQL implementation of FOPL Relational Model as an RDFLib Store.
 
 The MySQL store’s relational schema incorporates hashes of terms for efficient interning as well as other normalizations. Very little of it is written specifically for MySQL and thus theoretically it can be very easily ported to other back-ends (with the possible exception of issues with its use of foreign keys).
+
+Note: Python 2.X only.
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def setup_python3():
     from distutils.filelist import FileList
     from distutils import dir_util, file_util, util, log
     from os.path import join
-  
+
     tmp_src = join("build", "src")
     log.set_verbosity(1)
     fl = FileList()
@@ -27,9 +27,9 @@ def setup_python3():
         outf, copied = file_util.copy_file(f, join(tmp_src, f), update=1)
         if copied and outf.endswith(".py"):
             outfiles_2to3.append(outf)
-  
+
     util.run_2to3(outfiles_2to3)
-  
+
     # arrange setup to use the copy
     sys.path.insert(0, tmp_src)
 
@@ -37,6 +37,8 @@ def setup_python3():
 
 # Find version. We have to do this because we can't import it in Python 3 until
 # its been automatically converted in the setup process.
+
+
 def find_version(filename):
     _version_re = re.compile(r'__version__ = "(.*)"')
     for line in open(filename):
@@ -47,42 +49,46 @@ def find_version(filename):
 __version__ = find_version('rdflib_mysql/__init__.py')
 
 config = dict(
-    name = 'rdflib-mysql',
-    version = __version__,
-    description = "rdflib extension adding MySQL as back-end store",
-    author = "Graham Higgins",
-    author_email = "gjhiggins@gmail.com",
-    url = "http://github.com/RDFLib/rdflib-mysql",
-    packages = ["rdflib_mysql"],
-    download_url = "https://github.com/RDFLib/rdflib-mysql/zipball/master",
-    license = "BSD",
-    platforms = ["any"],
-    long_description = \
-    """
+    name='rdflib-mysql',
+    version=__version__,
+    description="rdflib extension adding MySQL as back-end store",
+    author="RDFLib Team",
+    maintainer="Graham Higgins",
+    maintainer_email="gjhiggins@gmail.com",
+    url="http://github.com/RDFLib/rdflib-mysql",
+    packages=["rdflib_mysql"],
+    download_url="https://github.com/RDFLib/rdflib-mysql/zipball/master",
+    license="BSD",
+    platforms=["any"],
+    long_description="""
     MySQL implementation of FOPL Relational Model as an RDFLib Store.
 
-    The MySQL store's relational schema incorporates hashes of terms for 
-    efficient interning as well as other normalizations. Very little of 
-    it is written specifically for MySQL and thus theoretically it can 
-    be very easily ported to other back-ends (with the possible exception 
+    The MySQL store's relational schema incorporates hashes of terms for
+    efficient interning as well as other normalizations. Very little of
+    it is written specifically for MySQL and thus theoretically it can
+    be very easily ported to other back-ends (with the possible exception
     of issues with its use of foreign keys).
     """,
-    classifiers = ["Programming Language :: Python",
-                   "Programming Language :: Python :: 2",
-                   "Programming Language :: Python :: 3",
-                   "Programming Language :: Python :: 2.4",
-                   "Programming Language :: Python :: 2.5",
-                   "Programming Language :: Python :: 2.6",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.2",
-                   "License :: OSI Approved :: BSD License",
-                   "Topic :: Software Development :: Libraries :: Python Modules",
-                   "Operating System :: OS Independent",
-                   "Natural Language :: English",
-                   ],
-    test_suite = "test",
-    install_requires = ["rdflib>=3.0", "rdfextras>=0.1", "MySQL-python>=1.2.3"],
-    entry_points = {
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        ],
+    test_suite="test",
+    install_requires=[
+            "rdflib>=3.0",
+            "rdfextras>=0.1",
+            "MySQL-python>=1.2.3"
+            ],
+    entry_points={
         'rdf.plugins.store': [
             'MySQL = rdflib_mysql.MySQL:MySQL',
         ],
@@ -96,10 +102,9 @@ if sys.version_info[0] >= 3:
 else:
     try:
         from setuptools import setup
-        config.update({'test_suite' : "nose.collector"})
+        config.update({'test_suite': "nose.collector"})
     except ImportError:
         from distutils.core import setup
 
 
 setup(**config)
-
